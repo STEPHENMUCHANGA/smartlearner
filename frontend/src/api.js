2b32cd33b78cbba7ef644c4e20cd7107d536220e
STEPHENMUCHANGA <stephenmuchanga@gmail.com>
2025-10-23 10:57:58 +0300

    Update files

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,17 @@
 import axios from "axios";
 
+// Dynamically choose backend depending on environment
+const baseURL =
+  import.meta.env.MODE === "development"
+    ? import.meta.env.VITE_API_URL
+    : import.meta.env.VITE_API_URL_PROD;
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api",
-  withCredentials: true,
+  baseURL,
+  withCredentials: true, // allows cookies for auth
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default api;
