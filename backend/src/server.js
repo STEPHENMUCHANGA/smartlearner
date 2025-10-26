2125091fe522875b038422cc9a58cbc2f035dfac
STEPHENMUCHANGA <stephenmuchanga@gmail.com>
2025-10-26 18:38:10 +0300

    update file

--- backend/src/server.js
+++ backend/src/server.js
@@ -38,14 +38,7 @@ const allowedOrigins = [
 ].filter(Boolean); // removes undefined values
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      console.log("❌ Blocked by CORS:", origin);
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: '*',
   credentials: true,
 }));
 app.use(express.json());
